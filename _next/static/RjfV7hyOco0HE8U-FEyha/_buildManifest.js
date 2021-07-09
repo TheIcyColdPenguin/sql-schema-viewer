4cb8dfee0016592d51c73c2f5ce7115e23689903
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2021-07-09 21:03:25 +0530

    github-pages

---
+++ _next/static/RjfV7hyOco0HE8U-FEyha/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f04ea036ba3cab50bdea.css","static/chunks/pages/index-ca2d8d09194b1b34abb4.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
