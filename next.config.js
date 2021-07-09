afe642c77169a9eceacf8ec913127fc2a5009db7
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2021-07-10 01:43:40 +0530

    update config to handle both dev and prod

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
-module.exports = {
-    basePath: '/sql-schema-viewer',
-    assetPrefix: '/sql-schema-viewer/',
-    trailingSlash: true,
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase, { defaultConfig }) => {
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        return {
+            trailingSlash: true,
+        };
+    }
+
+    return {
+        basePath: '/sql-schema-viewer',
+        assetPrefix: '/sql-schema-viewer/',
+        trailingSlash: true,
+    };
 };
