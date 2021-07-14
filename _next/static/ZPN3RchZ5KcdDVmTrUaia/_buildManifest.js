00a1efd3f89f4ff900977160c9d75150dd5c9201
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2021-07-15 02:08:14 +0530

    fix path for svg

---
+++ _next/static/ZPN3RchZ5KcdDVmTrUaia/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/129044335a480feecbd8.css","static/chunks/pages/index-789207ecb82a6b55190d.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
