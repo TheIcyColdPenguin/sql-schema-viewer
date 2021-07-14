e7913c638c4c94e69cdc3aa628aa49aae9e0ec14
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2021-07-15 00:52:40 +0530

    remove logs

---
+++ _next/static/4aHmVbS8dM-e0z22CgOyI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b311202a44cb131c7767.css","static/chunks/pages/index-97e54d4d22d1774a1964.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/jZaRaOjta4Ar8SazBfVlG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b311202a44cb131c7767.css","static/chunks/pages/index-7777ee9ff6b478c781ef.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
