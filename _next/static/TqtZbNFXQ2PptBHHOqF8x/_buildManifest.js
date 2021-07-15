1edafdf98aff11075b58f1de2cee88c163a5f437
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2021-07-15 19:32:31 +0530

    fix bug where incorrect column was getting its reference set

---
+++ _next/static/TqtZbNFXQ2PptBHHOqF8x/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b1944c74e702e63c7948.css","static/chunks/pages/index-a912b293f96b8aff8955.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
